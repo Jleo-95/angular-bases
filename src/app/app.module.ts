ea6d5ef96db0e4ebbcfca5e78a4c139902b68819
jorge leonardo aguilar <jorge.leonardo_89@hotmail.com>
2024-09-25 10:05:24 -0500

    coutryApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
+import { SharedModule } from './shared/shared.module';
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,10 +11,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    SharedModule
   ],
   providers: [
-    provideClientHydration()
+
   ],
   bootstrap: [AppComponent]
 })
